654dc54483a4f1d0fdfa064562c79f8a7c15ebb9
PaoloCrescenzi <crescenzipaolo2@gmail.com>
2023-01-19 16:54:19 +0100

    Fine esercizio + bonus

---
+++ resources/views/home.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container pt-4">
+        <div class="text-center">
+            <h1>HOME</h1>
+        </div>
+        <div class="text-center">
+            <a href="/about"><button class="btn btn-primary">About page</button></a>
+            <a href="/login"><button class="btn btn-primary">Login page</button></a>
+        </div>
+    </div>
+</body>
+</html>
