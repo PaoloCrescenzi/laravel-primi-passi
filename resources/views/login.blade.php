7de9823e3cf299687485fd3940c095f7a8c73ccc
PaoloCrescenzi <crescenzipaolo2@gmail.com>
2023-01-19 17:30:09 +0100

    Aggiunta form login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,10 +8,30 @@
     <title>Document</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
 </head>
+
 <body>
     <div class="container text-center pt-4">
         <h1>Login</h1>
+
+        <form action="">
+            <div class="container">
+                <div class="row flex-column align-items-center">
+                    <div class="form-outline mb-4 col-2">
+                        <input type="email" id="form2Example1" class="form-control" />
+                        <label class="form-label" for="form2Example1">Email address</label>
+                    </div>
+
+                    <div class="form-outline mb-4 col-2">
+                        <input type="password" id="form2Example2" class="form-control" />
+                        <label class="form-label" for="form2Example2">Password</label>
+                    </div>
+                </div>
+                <button type="button" class="btn btn-primary btn-block mb-4">Sign in</button>
+            </div>
+        </form>
+
         <a href="/"><button class="btn btn-danger">Home</button></a>
     </div>
 </body>
+
 </html>
